da66811cb3319aa9776eb4d1abc6baef852a5021
noa <s24006@std.it-college.ac.jp>
2024-12-10 15:49:03 +0900

    CH05-01 jsでHTMLのテキストを変更

---
+++ CH05/CH05-01/script.js
@@ -0,0 +1,5 @@
+//HTMLの中から、id = textの行を取得してelにいれる
+var el = document.getElementById('text');
+/*innerHTMLは、Elementオブジェクトが持つプロパティの一つで、
+要素が持つ情報を参照したり、変更したりすることができる*/
+el.innerHTML='Hello JavaScript';
